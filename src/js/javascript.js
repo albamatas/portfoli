ab1bed012d451a0519b5c89967692acaa3914d57
albamatas <albamatasvalls@gmail.com>
2017-04-21 18:19:07 +0200

    Updates

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -4,4 +4,14 @@ $( document ).ready(function() {
      $(".hamburger--elastic").toggleClass("is-active");
      $(".header").toggleClass("header-height");
         });
+
+        $(".cta").click( function(){
+          var divInfo= $(this).parent();
+	             $(this).siblings().show();
+               $(divInfo).css({
+                 "height": "auto"
+               });
+               $(divInfo).find("img, div").show();
+
+             });
  });
